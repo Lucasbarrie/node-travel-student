e93fb3bd5eba88e072a78ca0e7579aa05ca61723
lucas barrie <lucasbarrie@lucass-MacBook-Pro.local>
2014-01-12 18:27:27 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ appHelper.runApp(function(app, db) {
 	console.log("My app is running!");
 
 	app.get("/hello", function (req, resp) {
-		resp.write("HELLO WORLD!");
+		resp.write("HELLO WORLD! My name is Lucas");
 		resp.end();
+	});
+	app.get("/html", function (req, resp) {
+		resp.write("<html><body><h1>WOO html!</h1></body></html>");
+		resp.end();
+	});
+
+	// app.get("/template/:name/:food/", function (req, resp) {
+	// 	resp.render("template", { name: req.params.name, food: req.params.food });
+
+	app.get("/", function (req, resp) {
+		resp.render("index", { });
 	});
 })
